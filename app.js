7e31bfeeeacb9b18befada22bf13c4945d588591
Niseme <nijolesemele@gmail.com>
2022-06-30 09:08:35 +0300

    udates

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const cornerPic = document.querySelector('.corner-picture');
 
 
 
+
 const tl = new TimelineMax();
-tl.fromTo(bgr, 0.1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
-    .fromTo(bgr, 0.1, { width: "100%" }, { width: "80%", ease: Power2.easeInOut });
+// tl.fromTo(bgr, 0.1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
+//     .fromTo(bgr, 0.1, { width: "100%" }, { width: "80%", ease: Power2.easeInOut });
 
 tl.fromTo(wom, 1.2, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
     .fromTo(wom, 1.2, { width: "100%" }, { width: "100%", ease: Power2.easeInOut });
+
