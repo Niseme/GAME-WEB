7cd8299b20f8351783ab1957d42e0550f79cca87
Niseme <nijolesemele@gmail.com>
2022-06-30 08:50:49 +0300

    updates

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const bgr = document.querySelector('.bg-red');
 const wom = document.querySelector('.woman');
+const cornerPic = document.querySelector('.corner-picture');
+
 
 
 const tl = new TimelineMax();
-// tl.fromTo(bgr, 0.01, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
-//     .fromTo(bgr, 1.2, { width: "100%" }, { width: "80%", ease: Power2.easeInOut });
+tl.fromTo(bgr, 0.1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
+    .fromTo(bgr, 0.1, { width: "100%" }, { width: "80%", ease: Power2.easeInOut });
 
-
-
-tl.fromTo(wom, 0.1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut });
+tl.fromTo(wom, 1.2, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
+    .fromTo(wom, 1.2, { width: "100%" }, { width: "100%", ease: Power2.easeInOut });
